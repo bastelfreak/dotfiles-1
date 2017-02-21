e34e0083d04bbbfea2a170e5941fcc77ed47f25a
Tim Meusel <tim@bastelfreak.de>
2017-02-22 00:00:53 +0100

    puppet-lint: fix only_variable_string

--- modules/zsh/manifests/init.pp
+++ modules/zsh/manifests/init.pp
@@ -10,22 +10,22 @@ class zsh (
   package { $plugins: ensure => 'present' }
 
   file { "/home/${user}/.zshrc":
-    owner  => "${user}",
-    group  => "${user}",
+    owner  => $user,
+    group  => $user,
     mode   => "0644",
     source => "puppet:///modules/zsh/files/zshrc",
   }
 
   file { "/home/${user}/.zshrc.local":
-    owner  => "${user}",
-    group  => "${user}",
+    owner  => $user,
+    group  => $user,
     mode   => "0644",
     source => "puppet:///modules/zsh/files/zshrc.local",
   }
 
   file { "/home/${user}/.zshrc.pre":
-    owner  => "${user}",
-    group  => "${user}",
+    owner  => $user,
+    group  => $user,
     mode   => "0644",
     source => "puppet:///modules/zsh/files/zshrc.pre",
   }
