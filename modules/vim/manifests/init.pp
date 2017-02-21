0f228a12a11aa53bd9249a0e85d5494c6e60e09e
Christian Rebischke <Chris.Rebischke@posteo.de>
2017-02-21 23:32:04 +0100

    restructuring dotfiles repo as puppet directory

---
+++ modules/vim/manifests/init.pp
@@ -0,0 +1,23 @@
+class vim {
+  $plugins = [ 
+    'vim', 
+    'vim-airline', 
+    'vim-align', 
+    'vim-fugitive',
+    'vim-nerdcommenter', 
+    'vim-surround', 
+    'vim-syntastic', 
+    'vim-systemd', 
+    'vim-ultisnips' 
+  ]
+
+  package { $plugins: ensure => 'present' }
+
+  file { "/home/$user/.vimrc":
+    owner  => "$user",
+    group  => "$user",
+    mode   => "0644",
+    source => "puppet:///modules/vim/files/vimrc",
+  }
+
+}
---
+++ modules/zsh/manifests/init.pp
@@ -0,0 +1,29 @@
+class zsh {
+  $plugins = [ 
+    'zsh',
+    'grml-zsh-config',
+  ]
+
+  package { $plugins: ensure => 'present' }
+
+  file { "/home/$user/.zshrc":
+    owner  => "$user",
+    group  => "$user",
+    mode   => "0644",
+    source => "puppet:///modules/zsh/files/zshrc",
+  }
+
+  file { "/home/$user/.zshrc.local":
+    owner  => "$user",
+    group  => "$user",
+    mode   => "0644",
+    source => "puppet:///modules/zsh/files/zshrc.local",
+  }
+
+  file { "/home/$user/.zshrc.pre":
+    owner  => "$user",
+    group  => "$user",
+    mode   => "0644",
+    source => "puppet:///modules/zsh/files/zshrc.pre",
+  }
+}
